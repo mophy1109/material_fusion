205dbc3a60a2310ba88f637e1590562d43af8825
MophyChen <xxqqxx930519@gmail.com>
2018-03-28 16:59:04 +0800

    updated .gitignore some new functions

--- src/fusion.h
+++
@@ -1,6 +0,0 @@
-#ifndef FUSION_H
-#define FUSION_H
-
-#endif
-
-void fusionImages(vector<string> image_files);
--- src/preprocess.h
+++
@@ -1,8 +0,0 @@
-#ifndef PREPROCESS_H
-#define PREPROCESS_H
-
-
-void sampling(std::string src, std::string tar_dir);
-
-#endif
-
